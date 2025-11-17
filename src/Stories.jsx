f0bedb25fbe9b265cecb4fa436ee9c8045c57212
Gnanakumar01 <sgnanakumar929@gmail.com>
2025-11-17 22:28:24 +0530

    updated

--- src/Stories.jsx
+++ src/Stories.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Stories() {
+
+  const [stories, setStories] = useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:3000/stories')
+    .then(data => data.json())
+    .then(data => setStories(data))
+    .catch(err => console.log(err))
+  },[]);
+
   return (
-    <div className='stories bg-info'>Stories</div>
+    <div className='stories d-flex'>
+      {stories.length > 0 ?(
+        stories.map((stories)=>(
+          <div key={stories.id}>
+            <img src={stories.user.userImage} alt="dp" className='story_dp'/>
+          </div>
+
+        ))
+      ):(
+        <p>Loading</p>
+      )}
+    </div>
   )
 }
 
