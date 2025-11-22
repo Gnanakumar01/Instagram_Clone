e5bb4d43174b5eaed83f5fe32d6c24ef5ee50d8d
Gnanakumar01 <sgnanakumar929@gmail.com>
2025-11-22 21:37:44 +0530

    Updated

--- src/ViewStory.jsx
+++ src/ViewStory.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { data, useParams } from 'react-router-dom'
 
 function ViewStory() {
+
+  const {id} = useParams();
+
+  cosnt [story, setStory] = useState(null);
+
+  useEffect(()=>{
+    fetch(`http://localhost:5173/stories/${id}`)
+    .then(data => data.json())
+    .then(data => setStory(data))
+    .catch(err => console.log(err))
+  },[]);
   return (
-    <div>ViewStory</div>
+    <div>
+      {stories? <div>stories.user.username</div> : <div>Loading</div>}
+    </div>
   )
 }
 
